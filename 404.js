5ad3dd3c395f1e73c150837c1d7c93323c88aebf
ZainZafar <zainzafarsial@gmail.com>
2023-02-10 16:34:39 +0100

    added 404 logic

---
+++ 404.js
@@ -0,0 +1,21 @@
+var suffix = window.location.pathname.split("/")[1];
+var redirectTo;
+
+switch (suffix) {
+    case "note":
+        redirectTo = "http://zain.ovh/Note/note.html";
+        break;
+    case "quotes":
+        redirectTo = "http://zain.ovh/Quotes/quote.html";
+        break;
+    case "resume":
+    case "cv":
+        redirectTo = "http://zain.ovh/Resume/resume.html";
+        break;
+    default:
+        redirectTo = "http://zain.ovh";
+}
+
+setTimeout(function () {
+    window.location.href = redirectTo;
+}, 5000);
